12bbe43cc8890eceb10189e5029a0b8eaa4270e8
DPMK48 <kadaladorathy@gmail.com>
2026-06-11 18:12:19 +0100

    Auto-detect Render URL in paystack.ts

--- lib/paystack.ts
+++ lib/paystack.ts
@@ -5,6 +5,7 @@ const USE_STUB =
 
 const APP_BASE_URL =
   process.env.APP_BASE_URL ??
+  process.env.RENDER_EXTERNAL_URL ??
   (process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000");
